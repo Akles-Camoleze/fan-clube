043ee99ac40ee28d2d840f373fb4e8633929d87d
Akles Camoleze <aklespires@gmail.com>
2023-12-10 03:37:48 -0300

    Criada função para recuperação de inscrições

--- src/main/java/application/backend/entities/Inscricao.java
+++ src/main/java/application/backend/entities/Inscricao.java
@@ -1,11 +1,16 @@
 package application.backend.entities;
 
+import application.database.DbException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 public class Inscricao extends BaseEntity {
     private Date dataIncricao;
     private Usuario usuario;
     private Evento evento;
+    private Integer idUsuario;
+    private Integer idEvento;
 
     public Inscricao() {
     }
@@ -21,6 +26,16 @@ public class Inscricao extends BaseEntity {
         this.dataIncricao = dataIncricao;
         this.usuario = usuario;
         this.evento = evento;
+    }
+
+    public Inscricao(ResultSet resultSet) {
+        try {
+            id = resultSet.getInt("ins.id");
+            usuario = new Usuario(resultSet);
+            evento = new Evento(resultSet);
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     public Date getDataIncricao() {
@@ -45,5 +60,21 @@ public class Inscricao extends BaseEntity {
 
     public void setEvento(Evento evento) {
         this.evento = evento;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public Integer getIdEvento() {
+        return idEvento;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setIdEvento(Integer idEvento) {
+        this.idEvento = idEvento;
     }
 }
--- src/main/java/application/backend/repository/InscricaoRepository.java
+++ src/main/java/application/backend/repository/InscricaoRepository.java
@@ -1,8 +1,13 @@
 package application.backend.repository;
 
 import application.backend.dto.DataTransferObject;
+import application.backend.entities.Evento;
 import application.backend.entities.Inscricao;
+import application.database.DataBase;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,9 +35,57 @@ public class InscricaoRepository extends BaseRepository<Inscricao> {
         return new ArrayList<>();
     }
 
+    public List<Inscricao> findAllByUsuario(Integer idUsuario) {
+        return performOperation(connection -> {
+            PreparedStatement st = connection.prepareStatement("""
+                   
+                    SELECT `ins`.*, `usr`.*, `env`.*, `ps`.*, `end`.*, `cd`.*, `tp`.*
+                       FROM `fan_club`.`inscricao` as `ins`
+                       JOIN `fan_club`.`usuario` as `usr` ON `usr`.`id` = `ins`.`idUsuario`
+                       JOIN `fan_club`.`tipoUsuario` as `tp` ON `tp`.`id` = `usr`.`idTipoUsuario`
+                       JOIN `fan_club`.`evento` as `env` ON `env`.`id` = `ins`.`idEvento`
+                       JOIN `fan_club`.`pessoa` as `ps` ON `ps`.`id` = `usr`.`idPessoa`
+                       JOIN `fan_club`.`endereco` as `end` ON `end`.`id` = `ps`.`idEndereco`
+                       JOIN `fan_club`.`cidade` as `cd` ON `cd`.`id` = `end`.`idCidade`
+                       WHERE `usr`.`id` = ?;
+                    """
+            );
+            st.setInt(1, idUsuario);
+
+            ArrayList<Inscricao> inscricoes = new ArrayList<>();
+            ResultSet result = st.executeQuery();
+
+            while (result.next()) {
+                inscricoes.add(new Inscricao(result));
+            }
+            return inscricoes;
+        });
+    }
+
     @Override
     public Inscricao save(Inscricao entity) {
-        return  null;
+        return performOperation((connection -> {
+            PreparedStatement st = connection.prepareStatement("""
+                            INSERT INTO `fan_club`.`usuario` (idUsuario, idEvento)
+                            VALUES (?, ?)""",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            st.setInt(1, entity.getIdUsuario());
+            st.setInt(2, entity.getIdEvento());
+
+            if (st.executeUpdate() > 0) {
+                ResultSet resultSet = st.getGeneratedKeys();
+                if (resultSet.next()) {
+                    entity.setId(resultSet.getInt(1));
+                }
+                DataBase.closeResultSet(resultSet);
+                DataBase.closeStatement(st);
+                return entity;
+            }
+            DataBase.closeStatement(st);
+
+            return null;
+        }));
     }
 
 }
